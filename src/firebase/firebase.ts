9bad245ebb62b5f7a210e58860aa23121b01fdce
Usmon <usmonhakimov1999@gmail.com>
2023-05-05 21:28:43 +0900

    initialized firebase and add auth page

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDW2sNb0rqAl029Ob71SABDi3e0ZXrsg4",
+  authDomain: "my-movie-app-6bdf5.firebaseapp.com",
+  projectId: "my-movie-app-6bdf5",
+  storageBucket: "my-movie-app-6bdf5.appspot.com",
+  messagingSenderId: "421648726843",
+  appId: "1:421648726843:web:3c238f0dd44b6f59bd551e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,18 +1,93 @@
 import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
 
 const Auth = () => {
+  const [auth, setAuth] = useState<"signin" | "signup">("signin");
+  const toggleAuth = (state: "signin" | "signup") => {
+    setAuth(state);
+  };
   return (
-    <>
+    <div className="relative h-screen w-screen flex flex-col md:items-center md:justify-center">
       <Head>
         <title>Auth</title>
         <meta
           name="description"
-          content="For using our application, You should sign in first"
+          content="For watching movies you should sign to app"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-2xl font-bold text-center">Sign in</h1>
-    </>
+      <Image
+        src={"https://cdn.wallpapersafari.com/24/74/zgeTuV.jpg"}
+        fill
+        alt="bg"
+        className="object-cover !hidden sm:!inline opacity-60 -z-10"
+      />
+      <Image
+        src={"/Logo.svg"}
+        alt="Logo"
+        height={70}
+        width={70}
+        className="cursor-pointer object-contain absolute top-5 left-5"
+      />
+      <form className="relative mt-24 space-y-8 rounded flex flex-col bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <h2 className="text-4xl font-semibold uppercase">
+          {auth === "signin" ? "Sign In" : "Sign Up"}
+        </h2>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input className="input" type="text" placeholder="Enter your ID" />
+          </label>
+          <label className="inline-block w-full">
+            <input
+              className="input"
+              type="password"
+              placeholder="Enter Your Password"
+            />
+          </label>
+        </div>
+        {auth === "signin" ? (
+          <button
+            className="w-full bg-[#E10856] py-3 font-semibold uppercase"
+            type="submit"
+          >
+            Sign In
+          </button>
+        ) : (
+          <button
+            className="w-full bg-[#E10856] py-3 font-semibold uppercase"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        )}
+
+        {auth === "signin" ? (
+          <div className="text-[gray]">
+            No account yet?{" "}
+            <button
+              type="button"
+              className="text-white hover:underline"
+              onClick={() => toggleAuth("signup")}
+            >
+              Sign Up Now
+            </button>
+          </div>
+        ) : (
+          <div className="text-[gray]">
+            Already have account?{" "}
+            <button
+              type="button"
+              className="text-white hover:underline"
+              onClick={() => toggleAuth("signin")}
+            >
+              Sign In Now
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
