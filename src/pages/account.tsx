de4189a0ce8b795f958c0f663e24b682e70b5440
Usmon <usmonhakimov1999@gmail.com>
2023-07-07 21:12:56 +0900

    add dynamic subscription data

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,4 +1,8 @@
 import MembershipPlan from "@/components/membership-plan/membership-plan";
+import { Subscription } from "@/interfaces/app.interface";
+import { API_REQUEST } from "@/services/api.service";
+import moment from "moment";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,7 +10,7 @@ import React from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { MdOutlineSubscriptions } from "react-icons/md";
 
-const Account = () => {
+const Account = ({ subscription }: AccountProps) => {
   return (
     <>
       <Head>
@@ -39,16 +43,21 @@ const Account = () => {
           <div className="-ml-1 flex items-center gap-x-1 5">
             <MdOutlineSubscriptions className="w-5 h-5 text-red-500" />
             <p className="text-md font-semibold text-[#555]">
-              Member since 15 February, 2023
+              Member since{" "}
+              {moment(subscription.current_period_start * 1000).format(
+                "DD MMM, yyyy"
+              )}
             </p>
           </div>
         </div>
 
-        <MembershipPlan />
+        <MembershipPlan subscription={subscription} />
 
         <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:bordder-x-0 md:border-t md:border-b-0 md:pb-0">
           <h4 className="text-lg text-[gray]">Plan Details</h4>
-          <div className="col-span-2 font-medium">Premium</div>
+          <div className="col-span-2 font-medium">
+            {subscription.plan.nickname.toUpperCase()}
+          </div>
           <p className="cursor-pointer text-blue-500 hover:underline md:text-right">
             Change Plan
           </p>
@@ -66,3 +75,29 @@ const Account = () => {
 };
 
 export default Account;
+
+export const getServerSideProps: GetServerSideProps<AccountProps> = async ({
+  req,
+}) => {
+  const user_id = req.cookies.user_id;
+
+  if (!user_id) {
+    return {
+      redirect: { destination: "/auth", permanent: false },
+    };
+  }
+
+  const subscription = await fetch(
+    `${API_REQUEST.subscription}/${user_id}`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      subscription: subscription.subscription.data[0],
+    },
+  };
+};
+
+export interface AccountProps {
+  subscription: Subscription;
+}
